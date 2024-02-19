b188eb254e182c5525dfede32f727846b523d638
henrique-oliveiraa <148977691+henrique-oliveiraa@users.noreply.github.com>
2024-02-19 10:41:22 -0300

    Update Battle.ts

--- Battle.ts
+++ Battle.ts
@@ -8,8 +8,8 @@ import { House5 } from "./House5";
 
 
 
-const daknessHouse = new House1(,,,,,)
-const lightOfHouse = new House2(,,,,,)
-const whatherOfHouse = new House3(,,,,,)
-const flameOfHouse = new House4(,,,,,)
-const House = new House5(,,,,,)
+const daknessHouse = new House1("Stark","Winter is comming",1000,5,20000)
+const lightOfHouse = new House2("Tyrion Lannister","Never forget what you are, the rest of the world will not",5000,4,10000)
+const whatherOfHouse = new House3(" Lannisters","A Lannister always pays his debts",4000,6,40000)
+const flameOfHouse = new House4("Cersei Lannister","When you play the game of thrones, you win or you die",6000,5,70000)
+const House = new House5("Melisandre","The night is dark and full of terrors! ",7000,5,30000)
