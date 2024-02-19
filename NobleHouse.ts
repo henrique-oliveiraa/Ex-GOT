ac8467b65fd005b1b6b05e77ed8b984ffa636cf0
henrique-oliveiraa <148977691+henrique-oliveiraa@users.noreply.github.com>
2024-02-19 10:40:48 -0300

    Update NobleHouse.ts

--- NobleHouse.ts
+++ NobleHouse.ts
@@ -1,23 +1,30 @@
-export interface NobleHouse {
-  
-    nome:string
-    lema:number
-    quantidadedeDinheiro:number;
-    quantidadedeHerdeiro:number;
-    quantidadedeSoldado:number;
-    
-constructor (nome:string,lema:string,quantidadedeDinheiro:number,quantidadedeHerdeiro:number,quantidadedeSoldado:number) {
+export class NobleHouse {
 
-this.nome = nome;
-this.lema = lema;
-this.quantidadedeDinheiro = quantidadedeDinheiro;
-this.quantidadedeHerdeiro = quantidadedeHerdeiro;
-this.quantidadedeSoldado = quantidadedeSoldado;
+    nome: string
+    lema: string
+    quantidadedeDinheiro: number;
+    quantidadedeHerdeiro: number;
+    quantidadedeSoldado: number;
+
+    constructor(nome: string, lema: string, quantidadedeDinheiro: number, quantidadedeHerdeiro: number, quantidadedeSoldado: number) {
+
+    this.nome = nome;
+    this.lema = lema;
+    this.quantidadedeDinheiro = quantidadedeDinheiro;
+    this.quantidadedeHerdeiro = quantidadedeHerdeiro;
+    this.quantidadedeSoldado = quantidadedeSoldado;
+
+    }
+
+    addherdeiros(){
+        const arrayHerdeiro:string[] = []
+
+
+    }
+
+
 
 }
-
-}
-
 
 
 
