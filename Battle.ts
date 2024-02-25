273bc4ee78c925015451be6c6dea39f9aa934816
henrique-oliveiraa <148977691+henrique-oliveiraa@users.noreply.github.com>
2024-02-25 20:15:41 -0300

    Update Battle.ts

--- Battle.ts
+++ Battle.ts
@@ -12,4 +12,49 @@ const daknessHouse = new House1("Stark","Winter is comming",1000,5,20000)
 const lightOfHouse = new House2("Tyrion Lannister","Never forget what you are, the rest of the world will not",5000,4,10000)
 const whatherOfHouse = new House3(" Lannisters","A Lannister always pays his debts",4000,6,40000)
 const flameOfHouse = new House4("Cersei Lannister","When you play the game of thrones, you win or you die",6000,5,70000)
-const House = new House5("Melisandre","The night is dark and full of terrors! ",7000,5,30000)
+const House = new House5("Melisandre","The night is dark and full of terrors! ",7000,5<5000)
+
+function battleOFtheHouses(
+  darknessHouse: number,
+  lightOfHouse: number,
+  whatherOfHouse: number,
+  flameOfHouse: number,
+  comunHouse: number
+): void {
+  let phantomNumber: number = 0;
+  let index: number;
+  let armyNumber: Array<number> = [
+    darknessHouse,
+    lightOfHouse,
+    whatherOfHouse,
+    flameOfHouse,
+    comunHouse,
+  ];
+
+  for (let i = 1; i < armyNumber.length; i++) {
+    phantomNumber = armyNumber[i];
+          index = i;
+
+    while (index > 0 && armyNumber[index - 1] > phantomNumber) {
+      armyNumber[index] = armyNumber[index - 1];
+          index--;
+    }
+    armyNumber[index] = phantomNumber;
+  }
+         console.log(
+    `The winner of the battle has won with the army number of ${armyNumber[5]}`
+  );
+}
+
+
+
+battleOFtheHouses(
+  darknessHouse.quantidadeSoldados,
+  lightOfHouse.quantidadeSoldados,
+  whatherOfHouse.quantidadeSoldados,
+  flameOfHouse.quantidadeSoldados,
+  comunHouse.quantidadeSoldados,
+);
+
+
+
